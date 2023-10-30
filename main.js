6ea274a53408daa8b3054d1f61f9fc75d536829c
ifdc2308 <italodorea123@gmail.com>
2023-10-29 21:01:55 -0300

    blank

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-//function effect delete and add text about
-
 const textElement = document.getElementById("dynamic-text");
 const words = ["Freelancer", "Front-End", "Developer"]; // Array com os textos que você deseja exibir
 
 let currentIndex = 0; // Índice do texto atual
+let charIndex = 0; // Índice do caractere atual
+let isDeleting = false; // Se está apagando ou não
 
 function updateText() {
-    textElement.textContent = words[currentIndex]; // Define o texto com base no índice atual
-    currentIndex = (currentIndex + 1) % words.length; // Avança para o próximo texto
+    let currentWord = words[currentIndex]; // Palavra atual
+    let currentDisplayText = currentWord.substring(0, charIndex); // Texto atualmente exibido
+
+    if (isDeleting) {
+        // Se estiver apagando, diminua o índice do caractere
+        charIndex--;
+    } else {
+        // Se não estiver apagando, aumente o índice do caractere
+        charIndex++;
+    }
+
+    if (charIndex === currentWord.length) {
+        // Se todos os caracteres da palavra atual foram exibidos, comece a apagar
+        isDeleting = true;
+    } else if (charIndex === 0) {
+        // Se todos os caracteres da palavra atual foram apagados, avance para a próxima palavra e comece a adicionar
+        isDeleting = false;
+        currentIndex = (currentIndex + 1) % words.length;
+    }
+
+    textElement.textContent = currentDisplayText; // Atualize o texto exibido
+
+    let timeout = isDeleting ? 200 : 100; // Defina o tempo de espera com base em se está apagando ou não
+
+    setTimeout(updateText, timeout); // Chame a função após o tempo de espera
 }
 
-// Atualiza o texto a cada 2 segundos (2000 milissegundos)
-setInterval(updateText, 2000);
-
-// Chame a função inicialmente para exibir o primeiro texto
+// Chame a função inicialmente para começar o efeito
 updateText();
+
 
